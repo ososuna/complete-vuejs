dd20f975aae9332718f78ac11ecf2fae086e29fe
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-20 19:21:29 -0600

    Hello world in composition api

---
+++ composition-api/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  setup() {
+    const msg = 'Hello World'
+
+    return {
+      msg
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
