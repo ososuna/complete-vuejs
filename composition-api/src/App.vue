f8954890297411e2785c4b426a559e7f8ec21e9a
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-21 17:07:19 -0600

    Before and after with watch

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -1,46 +1,53 @@
 <template>
 <div>
   <button @click="increment">{{ count }}</button>
-  <button @click="increase('a')">{{ numbers.a }}</button>
-  <button @click="increase('b')">{{ numbers.b }}</button>
+  <button @click="a++">{{ a }}</button>
+  <button @click="b++">{{ b }}</button>
   <p>{{ total }}</p>
+  <div v-for="number in history" :key="number">
+    {{ number }}
+  </div>
 </div>
 </template>
 
 <script>
 
-import { ref, reactive, computed, watch, watchEffect } from 'vue'
+import { ref, computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'App',
   setup() {
+    
     // ref -> number, string
     const count = ref(0)
-    // reactive -> {}
-    const numbers = reactive({
-      a: 1,
-      b: 2
-    })
+    const a = ref(0)
+    const b = ref(0)
+    const history = ref([])
 
-    const increase = (n) => {
-      numbers[n]++
-    }
+    // reactive -> {}
 
     const increment = () => {
       count.value++
     }
 
     const total = computed( () => {
-      return count.value + numbers.a + numbers.b
+      return count.value + a.value + b.value
     })
 
+    // In watch effect you can just pass it whatever you want (general)
     watchEffect(() => {
-      console.log( numbers.a )
+      console.log( a.value )
     })
 
-    watch(numbers, (newVal) => {
-      console.log( newVal )
-      console.log (`a: ${newVal.a}. b: ${newVal.b}`)
+    // In watch you need to specify what you're going to watch (specific)
+    // If you'd like to compare the previous and new value you have to use ref
+    watch([a, b], ([newA, newB], [oldA, oldB]) => {
+      if (newA != oldA ) {
+        history.value.push(`A: ${oldA} -> ${newA}`)
+      }
+      if (newB != oldB ) {
+        history.value.push(`B: ${oldB} -> ${newB}`)
+      }
     })
 
     // If you want to call it immediatly when the component is rendered as well everytime it changes
@@ -52,10 +59,11 @@ export default {
     // })
 
     return {
+      a,
+      b,
       count,
-      increase,
+      history,
       increment,
-      numbers,
       total
     }
   }
