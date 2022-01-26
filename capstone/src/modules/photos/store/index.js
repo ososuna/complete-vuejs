af9cd52af21a89fb150b71d814bb92a9d92e9531
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-26 16:40:10 -0600

    Reusable layout

--- capstone/src/modules/photos/store/index.js
+++ capstone/src/modules/photos/store/index.js
@@ -1,14 +1,14 @@
-import state from './state'
-import * as mutations from './mutations'
-import * as actions from './actions'
-import * as getters from './getters'
+// import state from './state'
+// import * as mutations from './mutations'
+// import * as actions from './actions'
+// import * as getters from './getters'
 
-const photosModule =  {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters
-}
+// const photosModule =  {
+//     namespaced: true,
+//     state,
+//     mutations,
+//     actions,
+//     getters
+// }
 
-export default photosModule
+// export default photosModule
