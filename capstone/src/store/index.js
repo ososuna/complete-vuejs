9c369e6891046a3d9ba8d66e3ef5f14ab655da0a
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-26 17:25:08 -0600

    Remove photos module

--- capstone/src/store/index.js
+++ capstone/src/store/index.js
@@ -1,10 +1,8 @@
 import { createStore } from 'vuex'
 import albums from '@/modules/albums/store'
-import photos from '@/modules/photos/store'
 
 export default createStore({
   modules: {
-    albums,
-    photos
+    albums
   }
 })
