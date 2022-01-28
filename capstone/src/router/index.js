06acb568800216b12b099788b847c62021809d27
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-28 16:35:47 -0600

    Added routing

--- capstone/src/router/index.js
+++ capstone/src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-
+  {
+    path: '/',
+    component: () => import(/* webpackChunkName: "Auth Layout" */ '@/modules/albums/views/PhotoApp'),
+  }
 ]
 
 const router = createRouter({
